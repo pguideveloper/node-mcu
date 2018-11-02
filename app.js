666824a786bab430f143f92f0ba0fbf08bf924a9
Pedro <pedroguimaraes.contato@gmail.com>
2018-11-01 23:07:19 -0200

    Creating a view

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
+
+app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
-    res.send("Teste")
+    res.render("teste")
 })
 
 var port = process.env.PORT || 8080
