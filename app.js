f640cf142d90f7ad18a023155e4783f3eb3ec69c
pguideveloper <pedroguimaraes.contato@gmail.com>
2018-11-08 19:48:14 -0200

    NODE MCU

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const mqtt = require('mqtt')
+const client  = mqtt.connect('mqtt://test.mosquitto.org')
 
-
-app.set('views', path.join(__dirname, 'views'))
-app.set("view engine", "ejs")
-
-app.get("/", (req, res) => {
-    res.render("teste")
+client.on('connect', function (callback) {
+    console.log(callback)
+  console.log("teste")
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt from IFSPCARAGUATAUBA')
+    }
+  })
 })
 
-var port = process.env.PORT || 8080
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
 
-app.listen(port, function () {
-    console.log(`Example app listening on port ${port}`);
-});
+
+
+// const port = process.env.PORT || 8080
+
+// app.set('views', path.join(__dirname, 'views'))
+// app.set("view engine", "ejs")
+
+// app.get("/", (req, res) => {
+//     res.render("teste")
+// })
+
+// app.listen(port, function () {
+//     console.log(`Example app listening on port ${port}`);
+// });
